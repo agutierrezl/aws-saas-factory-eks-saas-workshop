9863cc0af89ca5adb49569fb6289c9e592716652
Aryam Gutierrez <aryaml@amazon.com>
2025-03-10 15:03:07 +0100

    standard tier now use local account images

--- tenant-artifacts/deploy-pooled.sh
+++ tenant-artifacts/deploy-pooled.sh
@@ -2,6 +2,7 @@ STACKS=$(aws cloudformation describe-stacks)
 USERPOOLID=$(echo $STACKS | jq -r '.Stacks[]?.Outputs[]? | select(.OutputKey=="PooledTenantUserPoolId") | .OutputValue')
 APPCLIENTID=$(echo $STACKS | jq -r '.Stacks[]?.Outputs[]? | select(.OutputKey=="PooledTenantAppClientId") | .OutputValue')
 REGION=$(aws configure get region)
+AWS_ACCOUNT_ID=$(aws sts get-caller-identity --region $REGION |jq -r ".Account")
 ELBURL=$(kubectl get svc -l istio=ingress -n istio-ingress -o json | jq -r '.items[0].status.loadBalancer.ingress[0].hostname')
 IAM_ROLE_ARN=$(echo $STACKS | jq -r '.Stacks[]?.Outputs[]? | select(.OutputKey=="RoleUsedByTVM") | .OutputValue')
 PRODUCTTABLE=$(echo $STACKS | jq -r '.Stacks[]?.Outputs[]? | select(.OutputKey=="ProductTable") | .OutputValue')
@@ -19,19 +20,19 @@ app:
   iamRoleArn: $IAM_ROLE_ARN
   
   product:
-    image: public.ecr.aws/o2b5n0j5/eks-saas-product:v1.1
+    image: $AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/aws-workshop/eks-saas-product
     productTable: $PRODUCTTABLE
     port: 80
     targetPort: 3005
     
   order:
-    image: public.ecr.aws/o2b5n0j5/eks-saas-order:v1.1
+    image: $AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/aws-workshop/eks-saas-order
     orderTable: $ORDERTABLE
     port: 80
     targetPort: 3010
   
   application:
-    image: public.ecr.aws/o2b5n0j5/eks-saas-application:latest
+    image: $AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/aws-workshop/eks-saas-application
     port: 80
     targetPort: 80
 EOF
